6fd8906eb676b283f3eb00a50154042b83e50f12
Felipe <fpizarro.ux@gmail.com>
2022-04-19 20:29:23 +0200

    navbarUpdate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Qué pahoó</h1>
+    <div>
+      <Navbar/>
     </div>
   );
 }
