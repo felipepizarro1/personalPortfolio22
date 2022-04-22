655fd8a8158e8e5f820792b0a4646aefa1e86f3b
Felipe <fpizarro.ux@gmail.com>
2022-04-22 19:42:19 +0200

    Hero Page Begin

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState} from 'react';
 import Video from '../../videos/video.mp4'
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements';
+import { Button } from '../ButtonElemens'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements';  
 
 const HeroSection = () => {
+   const [hover, setHover] = useState(false)
+
+   const onHover = () => {
+    setHover(!hover)
+
+   }
+
+
   return (
     <HeroContainer >
         <HeroBg>
             <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
         </HeroBg>
-
+        <HeroContent>
+            <HeroH1>Hello There!</HeroH1>
+            <HeroP>I'm Felipe Pizarro, Junior Front End Developer
+            </HeroP>
+            <HeroBtnWrapper>
+                <Button to="contact" onMouseEnter={onHover} onMouseLeave={onHover} >Contact {hover ? <ArrowForward /> : <ArrowRight/>}
+                </Button>
+            </HeroBtnWrapper>
+        </HeroContent>
     </HeroContainer>
   )
 }
